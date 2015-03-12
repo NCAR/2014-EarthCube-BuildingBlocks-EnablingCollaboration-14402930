0d423a635944786293e99e1f2b8b3dc312267f57
Benjamin Gross <mbgross@unavco.org>
2015-03-12 11:56:12 -0600

    add comments to doi script

--- UNAVCO/parseDataDoisNoRepeats.py
+++ UNAVCO/parseDataDoisNoRepeats.py
@@ -1,15 +1,12 @@
-#This python script reads the data DOI file I have sitting on my hard drive and makes RDF out of it, yay!
+#This python script reads the dataset file I have sitting on my hard drive and makes RDF out of it, yay!
+#The script creates random URIs unique to this script but does not check the database for collisions with pre-existing URIs!
 #Benjamin Gross 2015, mbgross@unavco.org
 
 import re
 import csv
 import random
 namespace = 'http://vivo.unavco.org/vivo/individual/'
-rdfFile = 'rdfexport.txt'
 usedURIs = []
-repeatNames = []
-repeatNames.append([]) #create a 2 dimensional list to hold names and URIs
-repeatNames.append([])
 
 
 f = open('dataDOIs.csv')
@@ -17,12 +14,12 @@ csv_f = csv.reader(f)
 next(csv_f, None)  # skip the headers
 
 
-with open('datasets.n3','w') as newfile:
+with open('datasets.txt','w') as newfile:
 
     for row in csv_f:
         doi = row[0]
         authors = row [1]
-        
+
         title = row[2].replace(',',', ')
         title = title.replace('\,',',')
         year = row[4]
@@ -66,31 +63,31 @@ with open('datasets.n3','w') as newfile:
             rank = authorlist.index(peeps)+1
 
             #First name
-            newfile.write('<http://vivo.unavco.org/vivo/individual/'+vcardName+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2006/vcard/ns#Name> .\n')
-            newfile.write('<http://vivo.unavco.org/vivo/individual/'+vcardName+'> <http://www.w3.org/2006/vcard/ns#givenName> "'+name[0]+'" .\n')
+            newfile.write('<'+namespace+vcardName+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2006/vcard/ns#Name> .\n')
+            newfile.write('<'+namespace+vcardName+'> <http://www.w3.org/2006/vcard/ns#givenName> "'+name[0]+'" .\n')
 
             #Last name
             if len(name)>1:
-                newfile.write('<http://vivo.unavco.org/vivo/individual/'+vcardName+'> <http://www.w3.org/2006/vcard/ns#familyName> "'+name[-1]+'" .\n')
+                newfile.write('<'+namespace+vcardName+'> <http://www.w3.org/2006/vcard/ns#familyName> "'+name[-1]+'" .\n')
 
             #Authorship
-            newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoAuthorship+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://vivoweb.org/ontology/core#Authorship> .\n')
-            newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoAuthorship+'> <http://vivoweb.org/ontology/core#relates> <http://vivo.unavco.org/vivo/individual/'+vcardIndividual+'> .\n')
-            newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoAuthorship+'> <http://vivoweb.org/ontology/core#rank> "'+str(rank)+'"^^<http://www.w3.org/2001/XMLSchema#int> .\n')
-            newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoDataset+'> <http://vivoweb.org/ontology/core#relatedBy> <http://vivo.unavco.org/vivo/individual/'+vivoAuthorship+'> .\n')
+            newfile.write('<'+namespace+vivoAuthorship+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://vivoweb.org/ontology/core#Authorship> .\n')
+            newfile.write('<'+namespace+vivoAuthorship+'> <http://vivoweb.org/ontology/core#relates> <'+namespace+vcardIndividual+'> .\n')
+            newfile.write('<'+namespace+vivoAuthorship+'> <http://vivoweb.org/ontology/core#rank> "'+str(rank)+'"^^<http://www.w3.org/2001/XMLSchema#int> .\n')
+            newfile.write('<'+namespace+vivoDataset+'> <http://vivoweb.org/ontology/core#relatedBy> <'+namespace+vivoAuthorship+'> .\n')
 
             #Vcard individual
-            newfile.write('<http://vivo.unavco.org/vivo/individual/'+vcardIndividual+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2006/vcard/ns#Individual> .\n')
-            newfile.write('<http://vivo.unavco.org/vivo/individual/'+vcardIndividual+'> <http://www.w3.org/2006/vcard/ns#hasName> <http://vivo.unavco.org/vivo/individual/'+vcardName+'> .\n')
+            newfile.write('<'+namespace+vcardIndividual+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2006/vcard/ns#Individual> .\n')
+            newfile.write('<'+namespace+vcardIndividual+'> <http://www.w3.org/2006/vcard/ns#hasName> <'+namespace+vcardName+'> .\n')
 
         #dateTime
-        newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoDateTime+'> <http://vivoweb.org/ontology/core#dateTime> "'+year+'-01-01T00:00:00"^^<http://www.w3.org/2001/XMLSchema#dateTime> .\n')
-        newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoDateTime+'> <http://vivoweb.org/ontology/core#dateTimePrecision> <http://vivoweb.org/ontology/core#yearPrecision> .\n')
-        newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoDateTime+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://vivoweb.org/ontology/core#DateTimeValue> .\n')
+        newfile.write('<'+namespace+vivoDateTime+'> <http://vivoweb.org/ontology/core#dateTime> "'+year+'-01-01T00:00:00"^^<http://www.w3.org/2001/XMLSchema#dateTime> .\n')
+        newfile.write('<'+namespace+vivoDateTime+'> <http://vivoweb.org/ontology/core#dateTimePrecision> <http://vivoweb.org/ontology/core#yearPrecision> .\n')
+        newfile.write('<'+namespace+vivoDateTime+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://vivoweb.org/ontology/core#DateTimeValue> .\n')
 
         #vivo:dataset
-        newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoDataset+'> <http://vivoweb.org/ontology/core#dateTimeValue> <http://vivo.unavco.org/vivo/individual/'+vivoDateTime+'> .\n')
-        newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoDataset+'> <http://www.w3.org/2000/01/rdf-schema#label> "'+title+'" .\n')
-        newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoDataset+'> <http://purl.org/ontology/bibo/doi> "'+doi+'" .\n')
-        newfile.write('<http://vivo.unavco.org/vivo/individual/'+vivoDataset+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://vivoweb.org/ontology/core#Dataset> .\n')
+        newfile.write('<'+namespace+vivoDataset+'> <http://vivoweb.org/ontology/core#dateTimeValue> <'+namespace+vivoDateTime+'> .\n')
+        newfile.write('<'+namespace+vivoDataset+'> <http://www.w3.org/2000/01/rdf-schema#label> "'+title+'" .\n')
+        newfile.write('<'+namespace+vivoDataset+'> <http://purl.org/ontology/bibo/doi> "'+doi+'" .\n')
+        newfile.write('<'+namespace+vivoDataset+'> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://vivoweb.org/ontology/core#Dataset> .\n')
 #        raw_input("Press Enter to continue...")
