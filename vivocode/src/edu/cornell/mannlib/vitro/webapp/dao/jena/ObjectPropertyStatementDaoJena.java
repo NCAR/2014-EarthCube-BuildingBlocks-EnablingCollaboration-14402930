85fd53e8ea4cadd9cd12bf7cbf20aa2b01fe814c
hudajkhan <hjk54@cornell.edu>
2015-12-08 15:51:55 -0500

    rdf update - removing query parsing error

--- vivocode/src/edu/cornell/mannlib/vitro/webapp/dao/jena/ObjectPropertyStatementDaoJena.java
+++ vivocode/src/edu/cornell/mannlib/vitro/webapp/dao/jena/ObjectPropertyStatementDaoJena.java
@@ -788,7 +788,15 @@ public class ObjectPropertyStatementDaoJena extends JenaBaseDao implements Objec
 			String rangeUri, Model constructedModel) {
 		// TODO Auto-generated method stub
 		Model returnModel = ModelFactory.createDefaultModel();
-		Query selectQuery = QueryFactory.create(queryString);
+		Query selectQuery = null;
+        try {
+            selectQuery = QueryFactory.create(queryString, Syntax.syntaxARQ);
+        } catch(Throwable th){
+            log.error("Could not create SPARQL query for query string. " + th.getMessage());
+            log.error(queryString);
+            throw new RuntimeException(th);
+        }
+		//Query selectQuery = QueryFactory.create(queryString);
 		System.out.println("Select Query is " + queryString);
 		Query newConstructQuery = selectQuery.cloneQuery();
 		//Now try and set the template
